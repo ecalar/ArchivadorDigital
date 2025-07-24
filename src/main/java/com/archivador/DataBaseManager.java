594d901f8325228b06b06e17db31a37045c91d23
Enrique <enriquecalar@gmail.com>
2025-07-24 17:24:54 +0200

    ArchivadorDigital

--- src/main/java/com/archivador/DataBaseManager.java
+++ src/main/java/com/archivador/DataBaseManager.java
@@ -1,9 +1,8 @@
 package com.archivador;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.*;
 
 public class DataBaseManager {
     private static final String DB_URL = "jdbc:h2:file:./data/archivadordb;DB_CLOSE_ON_EXIT=FALSE";
